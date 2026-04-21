0904c169fff38f2925fabbb0b9d080c3afd10b82
David Brochart <david.brochart@gmail.com>
2026-04-21 11:44:53 +0200

    debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from subprocess import CalledProcessError
+
 import pytest
 from anyio import run_process
 
@@ -5,9 +7,13 @@ from anyio import run_process
 @pytest.fixture
 async def git_repo(tmp_path) -> None:
     (tmp_path / "foo.txt").write_text("Hello, World!")
-    await run_process("git init".split(), cwd=tmp_path)
-    await run_process("git add foo.txt".split(), cwd=tmp_path)
-    await run_process("git commit -m 'First commit'", cwd=tmp_path)
+    try:
+        await run_process("git init".split(), cwd=tmp_path)
+        await run_process("git add foo.txt".split(), cwd=tmp_path)
+        await run_process("git commit -m 'First commit'", cwd=tmp_path)
+    except CalledProcessError as exc:
+        print(f"{exc=}")
+        raise
 
 
 @pytest.fixture
