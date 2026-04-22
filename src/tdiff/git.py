76e1548e98d1edc5c3e3d11347735580147884cd
David Brochart <david.brochart@gmail.com>
2026-04-22 17:44:51 +0200

    Some fixes

--- src/tdiff/git.py
+++ src/tdiff/git.py
@@ -1,6 +1,6 @@
 from subprocess import CalledProcessError
 
-from anyio import Path, create_task_group, run_process
+from anyio import Path, run_process
 
 
 async def get_diff_paths(
@@ -10,35 +10,33 @@ async def get_diff_paths(
     if original:
         args += original
     if modified:
-        args += f":{modified}"
+        args += f" {modified}"
     result = await run_process(f"git diff --name-only {args}", cwd=cwd)
     paths = result.stdout.decode().splitlines()
     return [Path(path) for path in paths]
 
 
-async def get_file_contents(
-    paths: list[Path], ref: str = "", cwd: str | None = None
-) -> list[str]:
-    contents = ["" for _ in paths]
-    async with create_task_group() as tg:
-        for i, path in enumerate(paths):
-            tg.start_soon(get_file_content, path, ref, cwd, contents, i)
-    return contents
-
-
 async def get_file_content(
-    path: Path, ref: str, cwd: str | None, contents: list[str | None], i: int
-) -> None:
+    path: Path, ref: str = "", *, temp_path: Path | None = None, cwd: str | None = None
+) -> str:
+    content = ""
     if not ref:
         if cwd is None:
             _path = path
         else:
             _path = Path(cwd) / path
-        content = await _path.read_text()
+        try:
+            content = await _path.read_text()
+        except Exception:  # likely a unicode decode error
+            content = f"Error loading file: {temp_path}"
     else:
         try:
             result = await run_process(f"git cat-file -p {ref}:{path}", cwd=cwd)
             content = result.stdout.decode()
-        except CalledProcessError:
+        except CalledProcessError:  # the file likely doesn't exist
             content = ""
-    contents[i] = content
+        except Exception:  # likely a unicode decode error
+            content = f"Error loading file: {temp_path}"
+    if (size := len(content)) > 100_000:
+        content = f"File too big ({size} bytes): {temp_path}"
+    return content
